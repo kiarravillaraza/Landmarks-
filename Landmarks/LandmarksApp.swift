caf00b0f2fc32aa47e2d0d87a7acc2d7bc085244
Kiarra Villaraza <kvillaraza@luc.edu>
2022-05-16 19:09:10 -0500

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Kiarra Villaraza on 5/16/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
